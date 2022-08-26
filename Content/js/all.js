6bbd34005320fc56cd82c6c21a77e395016c0369
HsuLeii <ok841113@gmail.com>
2022-08-26 11:22:24 +0800

    0826

--- Content/js/all.js
+++ Content/js/all.js
@@ -1,15 +1,15 @@
 //導入header
 $(document).ready(function () {
-  $('header').load('header.html');
+    $('header').load('header.html');
 });
 
 //顯示登出按鈕
 $(".nav_name").click(function () {
-  $(".nav_name_menu").toggle();
+    $(".nav_name_menu").toggle();
 });
 
 //答案選擇後加入active
-$(".answer_option").click(function(){
-$(this).siblings().removeClass("active");
-$(this).addClass("active");
+$(".answer_option").click(function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
 });
